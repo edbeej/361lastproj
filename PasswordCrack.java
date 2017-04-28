3753f72f298d86ef2dfa3e8a52eecb90c4dabf5d
Edwin <edbeej@gmail.com>
2017-04-28 04:36:08 -0500

    more letters

--- PasswordCrack.java
+++ PasswordCrack.java
@@ -18,7 +18,9 @@ public class PasswordCrack {
     static int foundNum = 0;
     static char[] addLetters = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o',
                                 'p','q','r','s','t','u','v','w','x','y','z','1','2','3','4',
-                                '5','6','7','8','9','0','!','@','$','&','^','*'};
+                                '5','6','7','8','9','0','!','@','$','&','^','*','%','`','?','#',
+                                'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q',
+                                'R','S','T','U','V','W','X','Y','Z'};
 
     public class mainThread extends Thread {
         private int mangle;
